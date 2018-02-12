2f35d0e2a7ed01a32f30a984d53ece3881eec1b9
Daniel Tal <daniel.tal@dome9.com>
2018-02-12 20:51:34 +0200

    Add files via upload

---
+++ src/com/hit/filter/ResponseTimerFilter.java
@@ -0,0 +1,58 @@
+package com.hit.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class ResponseTimerFilter
+ */
+public class ResponseTimerFilter implements Filter
+{
+	protected FilterConfig config;
+
+	/**
+	 * Default constructor.
+	 */
+	public ResponseTimerFilter()
+	{}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy()
+	{}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+	{
+		long startTime = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long totalTime = System.currentTimeMillis() - startTime;
+		String name = "servlet";
+		if (request instanceof HttpServletRequest)
+		{
+			name = ((HttpServletRequest)request).getRequestURI();
+		}
+
+		config.getServletContext().log(name + " took " + totalTime + " msec");
+	}
+
+	/**
+	 * 
+	 * @param fConfig
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException
+	{
+		this.config = fConfig;
+	}
+
+}
